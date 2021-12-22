e10aadba8069a7380f1268c4cbfb01b9bec6ebf7
Hugo <huseyin@gozutok.info>
2021-12-22 08:15:08 +0300

    app commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ToggleTheme, theme } from "./components/theme/ToggleTheme";
 import "./App.css";
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from "./components/theme/Theme";
 import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -9,40 +9,38 @@ import { MainContext } from "./components/context/userContext";
 
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import RouteTable from "./components/context/RouteTable";
-function ThemeFunction({children}) {
+function ThemeFunction({ children }) {
   const [toggleDark, setToggleDark] = useState(false);
 
-  const [cart,addCart] =  useState([]);
+  const [cart, addCart] = useState([]);
 
   const data = {
     toggleDark,
     setToggleDark,
-    cart,addCart
+    cart,
+    addCart,
   };
   useEffect(() => {
-    addCart(JSON.parse(localStorage.getItem('cart')));
-    setToggleDark(JSON.parse(localStorage.getItem('theme')));
+    addCart(JSON.parse(localStorage.getItem("cart")));
+    setToggleDark(JSON.parse(localStorage.getItem("theme")));
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify( cart));
-    localStorage.setItem('theme', JSON.stringify( toggleDark));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("theme", JSON.stringify(toggleDark));
   }, [cart, toggleDark]);
   return (
- 
-      <MainContext.Provider value={data}>
-        <ThemeProvider theme={toggleDark ? darkTheme : lightTheme}>
-          <CssBaseline />
+    <MainContext.Provider value={data}>
+      <ThemeProvider theme={toggleDark ? darkTheme : lightTheme}>
+        <CssBaseline />
 
         {children}
-        </ThemeProvider>
-      </MainContext.Provider>
+      </ThemeProvider>
+    </MainContext.Provider>
   );
 }
 
-
 function App() {
- 
   // useEffect(() => {
   //   addCart(JSON.parse(window.localStorage.getItem('cart')));
   // }, []);
@@ -51,14 +49,13 @@ function App() {
   //   window.localStorage.setItem('cart', cart);
   // }, [cart]);
 
-
-  
-
   return (
-     <React.Fragment> 
-      <ThemeFunction>    <RouteTable/></ThemeFunction>
-      </React.Fragment>
-   
+    <React.Fragment>
+      <ThemeFunction>
+        {" "}
+        <RouteTable />
+      </ThemeFunction>
+    </React.Fragment>
   );
 }
 
